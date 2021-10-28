639ccd5d1c067af74bcc7783a4ab120ee6b0ceae
Jhonatan Celso <jhonatancelso@gmail.com>
2021-10-28 17:04:49 -0300

    Firebase Auth - Testing

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+	imports: [
+	],
+	controllers: [
+	],
+	providers: [
+	],
+})
+export class CadastrosModule { }
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCEan-FqarSNxr0QUovJtWEAtUcMcPzqnI",
+//   authDomain: "autenticacao-v1.firebaseapp.com",
+//   projectId: "autenticacao-v1",
+//   storageBucket: "autenticacao-v1.appspot.com",
+//   messagingSenderId: "725305173213",
+//   appId: "1:725305173213:web:847c0380ce64bee426e734",
+//   measurementId: "G-YLHRDBNMW7"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
